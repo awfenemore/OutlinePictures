970c49fac18e62cbcc612150b0a45b54082971be
Adam Fenemore <awfenemore@gmail.com>
2018-07-11 10:55:59 +1200

    Added a sensitivity feature and a comparator. Better results, struggling with yellows at the moment

--- OutlinePicturesV1/OutlinePicturesV1/OutlinePictures.cs
+++ OutlinePicturesV1/OutlinePicturesV1/OutlinePictures.cs
@@ -10,6 +10,12 @@ namespace OutlinePicturesV1
     static class OutlinePictures
     {
 
+        /** This is a PERCENTAGE specificity to compare colours. That is, it should be between 0 and 100, (and I make no guarantees if it isnt)
+        * and realistically it should be between 1 and 10 for any decent results. It represents the percentage different that two colours' RGB
+        * values can be to still be considered the same. Ie, if Colour1 RGB is within X percentage of Colour2 RGB values */
+        private static readonly double COLOUR_SENSITIVITY = 1.5;
+
+
 
         /* This method is the crux of the algorithm. It takes an Image as input and returns a grey and black outline only representation
          * of the same image.  */
@@ -36,19 +42,20 @@ namespace OutlinePicturesV1
 
                     //Count for how many of the 4 pixels are different to the middle pixel
                     int DifferentCount = 0;
-                    if (MiddleColour != LeftColour)
+
+                    if (ColourEquals(MiddleColour, LeftColour))
                     {
                         DifferentCount += 1;
                     }
-                    if (MiddleColour != RightColour)
+                    if (ColourEquals(MiddleColour, RightColour))
                     {
                         DifferentCount += 1;
                     }
-                    if (MiddleColour != TopColour)
+                    if (ColourEquals(MiddleColour, TopColour))
                     {
                         DifferentCount += 1;
                     }
-                    if (MiddleColour != BottomColour)
+                    if (ColourEquals(MiddleColour, BottomColour))
                     {
                         DifferentCount += 1;
                     }
@@ -66,14 +73,35 @@ namespace OutlinePicturesV1
 
                 }
             }
-
-
-
             return (System.Drawing.Image) ReturnImage;
         }
 
+
+        /* This method compares two colours. It returns true if each RGB value of Colour1 is within COLOUR_SENSITIVITY percentage of Colour2 */
+        private static bool ColourEquals(Color Color1, Color Color2)
+        {
+            //Factors for comparison
+            double UpperFactor = 1.00 + (COLOUR_SENSITIVITY / 100);
+            double LowerFactor = 1.00 - (COLOUR_SENSITIVITY / 100);
+            //Console.WriteLine(UpperFactor);
+            //Console.WriteLine(LowerFactor);
+
+            //Return true when, for each of RGB, the second colour is between lower and upper bounds of first colour, OR vice versa
+            if (    ((Color1.R * LowerFactor) <= Color2.R) && (Color2.R <= (Color1.R * UpperFactor)) || ((Color2.R * LowerFactor) <= Color1.R) && (Color1.R <= (Color2.R * UpperFactor))  &&
+                    ((Color1.G * LowerFactor) <= Color2.G) && (Color2.G <= (Color1.G * UpperFactor)) || ((Color2.G * LowerFactor) <= Color1.G) && (Color1.G <= (Color2.G * UpperFactor))  &&
+                    ((Color1.B * LowerFactor) <= Color2.B) && (Color2.B <= (Color1.B * UpperFactor)) || ((Color2.B * LowerFactor) <= Color1.B) && (Color1.B <= (Color2.B * UpperFactor))   )
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
 
 
 
     }
 }
+
+
+//Next step is probably a cutoff point for pixels that are slightly different but not very different
